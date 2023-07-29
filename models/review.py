c4164fe87e2fa1aba2a7e07b24b49dede2ec9d0e
Evance Odoyo <odoyoevanc@gmail.com>
2023-07-29 20:20:19 +0300

    Add missing tablename

--- models/review.py
+++ models/review.py
@@ -17,6 +17,8 @@ class Review(BaseModel, Base):
         user_id (sqlalchemy String): The id of user doing review.
         text (sqlalchemy String): The review made.
     """
+    __tablename__ = "reviews"
+
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
