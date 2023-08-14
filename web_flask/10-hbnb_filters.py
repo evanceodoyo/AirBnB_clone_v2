e12e5446210473893e161dcd4e85097d7af63184
Evance Odoyo <odoyoevanc@gmail.com>
2023-08-15 00:39:56 +0300

    Dynamically add HBNB filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+Script that starts Flask web application.
+The application listens on 0.0.0.0, port 5000.
+"""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.amenity import Amenity
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    Remove the current SQLAlchemy Session.
+    """
+    storage.close()
+
+
+@app.route("/hbnb_filters")
+def hbnb_filters():
+    """
+    Renders html page with with State, City, and Amenity objects.
+    """
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    for amenity in amenities:
+        print("Amenity: ", amenity)
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
